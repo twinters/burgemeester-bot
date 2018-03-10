8b091a2b13667c1d552a9e981cd6ccc8fd07d341
T. Winters <info@thomaswinters.be>
2018-03-10 23:47:17 +0100

    Updated prototype to reject some invalid actions

--- src/main/java/be/thomaswinters/samson/burgemeester/BurgemeesterBot.java
+++ src/main/java/be/thomaswinters/samson/burgemeester/BurgemeesterBot.java
@@ -1,20 +1,46 @@
 package be.thomaswinters.samson.burgemeester;
 
+import be.thomaswinters.language.DutchSentenceSubjectReplacer;
+import be.thomaswinters.language.SubjectType;
 import be.thomaswinters.wikihow.WikiHowPageScraper;
+import stringmorpher.Decapitaliser;
 
 import java.io.IOException;
 
 public class BurgemeesterBot {
 
     private WikiHowPageScraper wikiHow = new WikiHowPageScraper();
+    private DutchSentenceSubjectReplacer subjectReplacer = new DutchSentenceSubjectReplacer();
 
     private String getRandomAction() throws IOException {
-        return wikiHow.scrapeRandomCard("nl").getTitle().toLowerCase();
+        String randomAction = null;
+        do {
+            if (randomAction != null) {
+                System.out.println("Rejected: " + randomAction);
+            }
+            randomAction = wikiHow.scrapeRandomCard("nl").getTitle().toLowerCase();
+            randomAction = Decapitaliser.decapitaliseFirstLetter(randomAction);
+        } while (!isValidAction(randomAction));
+
+        return fixAction(randomAction);
+    }
+
+    private String fixAction(String randomAction) throws IOException {
+        return subjectReplacer.replaceSecondPerson(randomAction, "zij", "hun", "hen", "zichzelf", SubjectType.THIRD_SINGULAR);
+    }
+
+    private boolean isValidAction(String title) {
+        return !containsCapitalisedLetters(title);
+    }
+
+    private boolean containsCapitalisedLetters(String input) {
+        return !input.toLowerCase().equals(input);
+
     }
 
     public String createRandomToespraak() throws IOException {
         String action = getRandomAction();
-        return "Aan allen die " + action + ", proficiat. Aan allen die niet " + action + ", ook profiat.";
+        return "Aan allen die " + action + ", proficiat.\nAan allen die niet " + action + ", ook profiat.";
     }
 
 
