ae247171d5849233605d44b5ba29df3f8f6ab71a
T. Winters <info@thomaswinters.be>
2018-03-12 01:58:05 +0100

    Updated build to work better on heroku

--- src/main/java/be/thomaswinters/samson/burgemeester/BurgemeesterBot.java
+++ src/main/java/be/thomaswinters/samson/burgemeester/BurgemeesterBot.java
@@ -68,7 +68,7 @@ public class BurgemeesterBot {
 
 
     public static void main(String[] args) throws IOException {
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println(new BurgemeesterBot().createRandomToespraak() + "\n\n");
 
         }
