9eaba9287d40d588707bf43898050df315f11a02
T. Winters <info@thomaswinters.be>
2018-03-30 13:47:37 +0200

    Added replying system. Made it log in as well.

--- src/main/java/be/thomaswinters/samson/burgemeester/BurgemeesterBot.java
+++ src/main/java/be/thomaswinters/samson/burgemeester/BurgemeesterBot.java
@@ -1,10 +1,13 @@
 package be.thomaswinters.samson.burgemeester;
 
+import be.thomaswinters.bot.IChatBot;
 import be.thomaswinters.bot.ITextGeneratorBot;
+import be.thomaswinters.bot.data.IChatMessage;
 import be.thomaswinters.language.SubjectType;
 import be.thomaswinters.language.dutch.DutchSentenceSubjectReplacer;
 import be.thomaswinters.language.stringmorpher.Decapitaliser;
 import be.thomaswinters.samson.burgemeester.util.DutchActionNegator;
+import be.thomaswinters.sentence.SentenceUtil;
 import be.thomaswinters.textgeneration.domain.constraints.LockConstraint;
 import be.thomaswinters.textgeneration.domain.context.ITextGeneratorContext;
 import be.thomaswinters.textgeneration.domain.context.TextGeneratorContext;
@@ -14,37 +17,98 @@ import be.thomaswinters.textgeneration.domain.generators.named.NamedGeneratorReg
 import be.thomaswinters.twitter.GeneratorTwitterBot;
 import be.thomaswinters.twitter.bot.TwitterBot;
 import be.thomaswinters.twitter.bot.TwitterBotExecutor;
+import be.thomaswinters.twitter.util.TwitterUtil;
 import be.thomaswinters.wikihow.WikiHowPageScraper;
+import be.thomaswinters.wikihow.WikihowLoginCookieCreator;
+import be.thomaswinters.wikihow.WikihowSearcher;
+import be.thomaswinters.wikihow.data.PageCard;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-public class BurgemeesterBot implements ITextGeneratorBot {
+public class BurgemeesterBot implements ITextGeneratorBot, IChatBot {
 
+    private static final String NEDERLANDS = "nl";
+    private WikiHowPageScraper wikiHow = new WikiHowPageScraper(NEDERLANDS);
+    private WikihowSearcher wikiHowSearcher;
 
-    private WikiHowPageScraper wikiHow = new WikiHowPageScraper();
     private DutchSentenceSubjectReplacer subjectReplacer = new DutchSentenceSubjectReplacer();
     private DutchActionNegator negator = new DutchActionNegator();
     private final ITextGenerator toespraakGenerator;
 
     public BurgemeesterBot(ITextGenerator toespraakGenerator) {
         this.toespraakGenerator = toespraakGenerator;
+
+        this.wikiHowSearcher = createWikihowSearcher();
+    }
+
+    private WikihowSearcher createWikihowSearcher() {
+        String cookie = null;
+        try {
+            if (System.getenv("wikihow_user") != null && System.getenv("wikihow_password") != null) {
+                cookie = new WikihowLoginCookieCreator().login(
+                        System.getenv("wikihow_user"),
+                        System.getenv("wikihow_password"));
+            } else {
+                System.out.println("No wikihow login specified");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new WikihowSearcher(NEDERLANDS, cookie);
     }
 
     private String getRandomAction() throws IOException {
         String randomAction = null;
         do {
-            randomAction = wikiHow.scrapeRandomCard("nl").getTitle();
+            randomAction = wikiHow.scrapeRandomCard().getTitle();
             randomAction = Decapitaliser.decapitaliseFirstLetter(randomAction);
         } while (!isValidAction(randomAction));
 
         return fixAction(randomAction);
     }
 
+    private Optional<String> getActionRelevantTo(String message) {
+        String searchQuery = SentenceUtil.splitOnSpaces(message)
+                .filter(e -> !TwitterUtil.isTwitterWord(e))
+                .map(e -> SentenceUtil.removePunctuations(e))
+                .filter(SentenceUtil::hasOnlyLetters)
+                .collect(Collectors.joining(" "));
+
+        System.out.println("Searching on WikiHow for: " + searchQuery);
+
+        List<PageCard> pages = null;
+        try {
+            pages = wikiHowSearcher.search(searchQuery);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+
+        return pages.stream()
+                .map(PageCard::getTitle)
+                .map(Decapitaliser::decapitaliseFirstLetter)
+                .map(this::tryFixingAction)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst();
+    }
+
     // FIXERS
+    private Optional<String> tryFixingAction(String action) {
+        try {
+            return Optional.of(fixAction(action));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
     private String fixAction(String randomAction) throws IOException {
         return replaceSubject(removeBetweenBrackets(randomAction));
     }
@@ -73,11 +137,8 @@ public class BurgemeesterBot implements ITextGeneratorBot {
         return new TextGeneratorContext(new ArrayList<LockConstraint>(), register, true);
     }
 
-    public String createRandomToespraak() throws IOException {
-        String action = getRandomAction();
-
-
-        return toespraakGenerator.generate(createGenerationContext(getRandomAction()));
+    public String createToespraak(String action) throws IOException {
+        return toespraakGenerator.generate(createGenerationContext(action));
 
 //        return "Aan allen die " + action + ": proficiat.\nAan allen die " + negateAction(action) + ": ook proficiat.";
     }
@@ -85,7 +146,7 @@ public class BurgemeesterBot implements ITextGeneratorBot {
     @Override
     public Optional<String> generateText() {
         try {
-            return Optional.of(createRandomToespraak());
+            return Optional.of(createToespraak(getRandomAction()));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -93,9 +154,25 @@ public class BurgemeesterBot implements ITextGeneratorBot {
     }
 
     public static void main(String[] args) throws IOException, TwitterException {
-        TwitterBot bot = new GeneratorTwitterBot(TwitterFactory.getSingleton(), new BurgemeesterBotLoader().build());
+        BurgemeesterBot burgemeesterBot = new BurgemeesterBotLoader().build();
+        TwitterBot bot = new GeneratorTwitterBot(TwitterFactory.getSingleton(), burgemeesterBot, burgemeesterBot);
         new TwitterBotExecutor(bot).run(args);
     }
 
 
+    @Override
+    public Optional<String> generateReply(IChatMessage message) {
+        try {
+            Optional<String> relevantAction = getActionRelevantTo(message.getMessage());
+
+            if (relevantAction.isPresent()) {
+                return Optional.of(createToespraak(relevantAction.get()));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+
+    }
 }
